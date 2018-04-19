687954c1bbf1af1bfd9277842dd8f56cb4ad8d0d
Diego Yanez <diegosyanez92@gmail.com>
2018-04-19 18:22:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-#require 'pry'
+##require 'pry'
 
 class Song
   extend Memorable
@@ -11,15 +11,15 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@songs.detect{|a| a.name == name}
+  #end
 
   def artist=(artist)
     @artist = artist
   end
 
-  
+
   def to_param
     name.downcase.gsub(' ', '-')
   end
